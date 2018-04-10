0d9362f558e2f7da73451fec7729e47683ff5952
DESKTOP-E9O24O9\PC <anthony.dam91@gmail.com>
2018-04-10 07:31:39 -0500

    v1.1

---
+++ js/app.js
@@ -0,0 +1,69 @@
+
+var numero0 = 0
+numero1 = 0;
+
+function getDisplay() {
+    document.getElementById("display").textContent;
+}
+function setDisplay(val) {
+    document.getElementById("display").textContent = val;
+}
+
+document.getElementById("on").onclick = function () {
+    setDisplay("0");
+};
+document.getElementById("sign").onclick = function () {
+    alert("on");
+};
+document.getElementById("raiz").onclick = function () {
+    alert("on");
+};
+document.getElementById("dividido").onclick = function () {
+    alert("on");
+};
+
+document.getElementById("7").onclick = function () {
+    setDisplay(7);
+};
+document.getElementById("8").onclick = function () {
+    setDisplay("8");
+};
+document.getElementById("9").onclick = function () {
+    alert("on");
+};
+document.getElementById("por").onclick = function () {
+    alert("on");
+};
+
+document.getElementById("4").onclick = function () {
+    alert("on");
+};
+document.getElementById("5").onclick = function () {
+    alert("on");
+};
+document.getElementById("6").onclick = function () {
+    alert("on");
+};
+document.getElementById("menos").onclick = function () {
+    alert("on");
+};
+
+document.getElementById("1").onclick = function () {
+    alert("on");
+};
+document.getElementById("2").onclick = function () {
+    alert("on");
+};
+document.getElementById("3").onclick = function () {
+    alert("on");
+};
+
+document.getElementById("0").onclick = function () {
+    alert("on");
+};
+document.getElementById("punto").onclick = function () {
+    alert("on");
+};
+document.getElementById("igual").onclick = function () {
+    alert("on");
+};
